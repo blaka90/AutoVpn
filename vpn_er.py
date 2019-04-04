c4ae0a6df7d8fcc1b95eeda4616c3d694071e678
blaka90 <blaka90@hotmail.co.uk>
2019-04-04 22:21:53 +0100

    mostly working version, stopping vpn needs to be thread too

--- vpn_er.py
+++ vpn_er.py
@@ -45,26 +45,48 @@ class Window(QWidget):
 		self.vpn_auth_password = "533d2ve"
 		self.password = self.get_password()
 		self.init_ui()
+		self.print_ip()
+		self.type_vpn = ""
+		self.thread = QThreadPool()
 
 	# initilize the user interface
 	def init_ui(self):
-		"""all below code is (roughly) in decending visual order"""
-
 		# set the name, icon and size of main window
 		self.setWindowTitle("AutoVpn")
 		# self.setWindowIcon(QIcon("resources/syncer.png"))
-		self.setGeometry(150, 100, 200, 200)
+		self.setGeometry(150, 100, 200, 100)
 		# self.setStyleSheet("background-color: black")
+
+		self.ip_label = QLabel(self)
+
+		self.refresh_ip = QPushButton("refresh")
+		self.refresh_ip.clicked.connect(self.print_ip)
+
+		self.pl = QRadioButton("Poland")
+		self.pl.setChecked(True)
+		self.de = QRadioButton("Germany")
+
+		self.tcp80 = QRadioButton("tcp80")
+		self.tcp80.setChecked(True)
+		self.tcp443 = QRadioButton("tcp443")
+		self.udp53 = QRadioButton("udp53")
+		self.udp25000 = QRadioButton("udp25000")
+
+		self.conn_types = QButtonGroup(self)
+		self.conn_types.addButton(self.tcp80)
+		self.conn_types.addButton(self.tcp443)
+		self.conn_types.addButton(self.udp53)
+		self.conn_types.addButton(self.udp25000)
 
 		self.vpn_on = QPushButton("ON")
 		self.vpn_on.setCheckable(True)
-		self.vpn_on.setStyleSheet('background-color: green; color: black')
+		self.vpn_on.setStyleSheet('color: black')
 		self.vpn_on.setFixedWidth(100)
 		self.vpn_on.clicked.connect(self.start_vpn)
 
 		self.vpn_off = QPushButton("OFF")
 		self.vpn_off.setCheckable(True)
-		self.vpn_off.setStyleSheet('background-color: darkred; color: black')
+		self.vpn_off.setStyleSheet('color: black')
 		self.vpn_off.setFixedWidth(100)
 		self.vpn_off.clicked.connect(self.stop_vpn)
 
@@ -72,7 +94,24 @@ class Window(QWidget):
 		h_box.addWidget(self.vpn_on)
 		h_box.addWidget(self.vpn_off)
 
-		self.setLayout(h_box)
+		h_radio = QHBoxLayout()
+		h_radio.addWidget(self.pl)
+		h_radio.addWidget(self.de)
+
+		h_radio_types = QHBoxLayout()
+		h_radio_types.addWidget(self.tcp80)
+		h_radio_types.addWidget(self.tcp443)
+		h_radio_types.addWidget(self.udp53)
+		h_radio_types.addWidget(self.udp25000)
+
+		v_box = QVBoxLayout()
+		v_box.addWidget(self.ip_label)
+		v_box.addWidget(self.refresh_ip)
+		v_box.addLayout(h_radio)
+		v_box.addLayout(h_radio_types)
+		v_box.addLayout(h_box)
+
+		self.setLayout(v_box)
 
 	@staticmethod
 	def start_style():
@@ -104,8 +143,7 @@ class Window(QWidget):
 		os.chdir(dir_name)
 
 	def get_password(self):
-		ask_pass, ok_pressed = QInputDialog.getText(self, "VPN Authorization", "Sudo Password: ",
-		                                            QLineEdit.Password, "")
+		ask_pass, ok_pressed = QInputDialog.getText(self, "VPN Authorization", "Sudo Password: ", QLineEdit.Password, "")
 		if ok_pressed:
 			self.password = ask_pass
 			return self.password
@@ -125,24 +163,79 @@ class Window(QWidget):
 		if os.path.isfile(self.vpn_pl) and not os.path.isfile("vpnbook-de4-udp25000.ovpn"):
 			self.unzipper(self.vpn_de)
 
+	def get_vpn_options(self):
+		if self.pl.isChecked():
+			if self.tcp80.isChecked():
+				self.type_vpn = "vpnbook-pl226-tcp80.ovpn"
+			elif self.tcp443.isChecked():
+				self.type_vpn = "vpnbook-pl226-tcp443.ovpn"
+			elif self.udp53.isChecked():
+				self.type_vpn = "vpnbook-pl226-udp53.ovpn"
+			elif self.udp25000.isChecked():
+				self.type_vpn = "vpnbook-pl226-udp25000.ovpn"
+		elif self.de.isChecked():
+			if self.tcp80.isChecked():
+				self.type_vpn = "vpnbook-de4-tcp80.ovpn"
+			elif self.tcp443.isChecked():
+				self.type_vpn = "vpnbook-de4-tcp443.ovpn"
+			elif self.udp53.isChecked():
+				self.type_vpn = "vpnbook-de4-udp53.ovpn"
+			elif self.udp25000.isChecked():
+				self.type_vpn = "vpnbook-de4-udp25000.ovpn"
+
 	@staticmethod
 	def unzipper(fn):
 		zfile = zipfile.ZipFile(fn, 'r')
 		zfile.extractall()
 		zfile.close()
 
-	@staticmethod
-	def print_ip():
+	def print_ip(self):
 		my_ip = requests.get("http://ipecho.net/plain?")
 		if str(my_ip) == "<Response [200]>":
+			self.ip_label.setText("Current IP: " + my_ip.text)
 			print("\nCurrent ip:\n")
 			print(my_ip.text)
 			print("\n")
 
 	# this needs to be a thread
 	def start_vpn(self):
-		shell = pexpect.spawn("sudo openvpn --config vpnbook-de4-udp25000.ovpn")
-		# shell = pexpect.spawn("sudo openvpn --config vpnbook-pl226-tcp443.ovpn")
+		self.vpn_on.setStyleSheet('background-color: green; color: black')
+		self.vpn_off.setStyleSheet('background-color: darkred; color: black')
+		self.get_vpn_options()
+		command = "sudo openvpn --config " + self.type_vpn
+		self.sv = StartVpn(command, self.password, self.vpn_auth_name, self.vpn_auth_password)
+		self.thread.start(self.sv)
+
+	def stop_vpn(self):
+		self.vpn_off.setStyleSheet('background-color: green; color: black')
+		self.vpn_on.setStyleSheet('background-color: darkred; color: black')
+		p_name = "openvpn"
+		p = subprocess.Popen(['pidof', p_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		out, err = p.communicate()
+		print(out.decode())
+		shell = pexpect.spawn("sudo kill {}".format(out.decode()))
+		shell.expect('.*password for .*?: ')
+		shell.sendline(self.password)
+		shell.expect(pexpect.EOF, timeout=None)
+		cmd_show_data = shell.before
+		cmd_output = cmd_show_data.split(b'\r\n')
+		for data in cmd_output:
+			print(data.decode())
+		print("\n[*]--VPN Process Killed--\n")
+		self.print_ip()
+
+
+class StartVpn(QRunnable):
+
+	def __init__(self, command, password, vpn_name, vpn_password):
+		QRunnable.__init__(self)
+		self.command = command
+		self.password = password
+		self.vpn_auth_name = vpn_name
+		self.vpn_auth_password = vpn_password
+
+	def run(self):
+		shell = pexpect.spawn(self.command)
 		shell.expect('.*password for .*?: ')
 		shell.sendline(self.password)
 		shell.expect("Enter Auth Username:")
@@ -154,21 +247,6 @@ class Window(QWidget):
 		cmd_output = cmd_show_data.split(b'\r\n')
 		for data in cmd_output:
 			print(data.decode())
-
-	def stop_vpn(self):
-		p_name = "openvpn"
-		p = subprocess.Popen(['pidof', p_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		out, err = p.communicate()
-		shell = pexpect.spawn("sudo kill {}".format(out.decode()))
-		shell.expect('.*password for .*?: ')
-		shell.sendline(self.password)
-		shell.expect(pexpect.EOF, timeout=None)
-		cmd_show_data = shell.before
-		cmd_output = cmd_show_data.split(b'\r\n')
-		for data in cmd_output:
-			print(data.decode())
-		print("\n[*]--VPN Process Killed--\n")
-		sys.exit(7)
 
 """
 def main():
